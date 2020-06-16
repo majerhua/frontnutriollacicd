7c302458090c868cecc6aca6896eec26f16bc166
cmajerhua <carlos.majerhua@unmsm.edu.pe>
2020-06-15 22:48:32 -0500

    agregando lista de grupos alimenticios

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,257 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <v-container>
+  <v-row>
+    <div class="container-title">
+      <h1>NutriOlla</h1>
+      <p>Calculadora nutricional peruana</p>
+    </div>
+  </v-row>
+  <v-row>
+    <v-col
+      cols="12"
+      sm="8"
+      md="7"
+      class="alimentos"
+    >
+      <h2>¿Qué comistes hoy?</h2>
+      <p>Puedes agregar o editar tus alimentos</p>
+      <v-card 
+        class="card mx-auto"
+        max-width="100%"
+        outlined
+      >
+        <v-list-item three-line>
+          <v-list-item-content>
+            <v-list-item-title class="headline mb-1" style="font-size:1em !important;display:flex;justify-content:space-between;">
+              Desayuno 
+              <v-btn small color="success" @click="addAlimento('desayuno')"><v-icon>add_box</v-icon></v-btn></v-list-item-title>
+            <div>
+              <v-row v-for="cAd in cantidadAlimentosDesayuno"
+              :key="cAd.key"
+              >
+                <v-col
+                :cols="5"
+                >
+                  <v-select 
+                    class="font"
+                    :items="alimentos1"
+                    item-text="Nombre"
+                    item-value="Id"
+                    label="Alimento"
+                    @change="changeAlimento('desayuno',cAd.key,$event)"          
+                  ></v-select>
+                </v-col>
+                <v-col
+                :cols="2"
+                >
+                  <v-text-field 
+                    label="Unidad"
+                    class="font"
+                    value="0"               
+                  >
+                  </v-text-field>
+                </v-col>
+                <v-col
+                :cols="5"
+                >
+                  <div class="circle tipo_alimento">{{cAd.alimento.GrupoAlimenticio}}</div>
+                </v-col>
+              </v-row>
+            </div>
+          </v-list-item-content>
+        </v-list-item>
+      </v-card>
+    </v-col>
+    <v-col
+      cols="12"
+      sm="4"
+      md="5"
+    >
+    <div
+      class="resultados"
+    >
+      <div>
+        <h2>Resultados</h2>
+      </div>
+      <div>
+        <v-row>
+          <v-col>
+            <v-select 
+              class="font"
+              :items="sexos"
+              item-text="Descripcion"
+              item-value="Id"
+              v-model="selectedSexo"
+              label="Sexo"
+              ></v-select>
+          </v-col>
+          <v-col>
+            <v-select 
+              yclass="font"
+              :items="rangoEdades"
+              item-text="Descripcion"
+              item-value="Id"
+              label="Rango Edad"
+              v-model="selectedRangoEdad"          
+            ></v-select>
+          </v-col>
+        </v-row>
+      </div>
+    </div>
+    <div>
+      <p>Estos son los alimentos de tu alintención diaria basados en tu edady sexo.</p>
+    </div>
+    <div>
+      <v-row>
+        <v-col
+          :cols="8"
+          >
+        <div  class="container_grupo_alimenticio">
+          <div></div>
+          <div>CEREALES, TUBÉRCULOS Y LUGUMINOSAS</div>
+        </div>
+        </v-col>
+        <v-col
+            :cols="4"
+        >
+            <div class="circle">CORRECTO</div>
+        </v-col>
+      </v-row>
+    </div>
+    <div>
+      <v-row>
+        <v-col
+        :cols="8"
+        >
+          <div class="container_grupo_alimenticio">
+            <div></div>
+            <div >FRUTAS</div>
+          </div>
+        </v-col>
+        <v-col
+          :cols="4"
+        >
+          <div class="circle">CORRECTO</div>
+        </v-col>
+      </v-row>
+    </div>
+    </v-col>
+  </v-row>
+  </v-container>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+
+  data(){
+    return {
+      sexos: [],
+      rangoEdades:[],
+      cantidadAlimentosDesayuno:[],
+      alimentos1:[],
+      selectedSexo:0,
+      selectedRangoEdad:0
+    }
+  },
+  methods:{
+    getGruposNutricionales: function () {    
+      var vm = this;
+      axios.get("https://calculadoranutricional.herokuapp.com/api/consumo-estimado",{
+              params: {
+                idSexo: vm.selectedSexo,
+                idRangoEdad:vm.selectedRangoEdad
+              }
+      })
+      .then(function(res){
+        vm.gruposNutricionales = res.data.data;
+        vm.Kcal = res.data.data.kcalEstimado;
+      })
+    },
+    changeAlimento: function(pTipo,pKey,value){
+      var vm = this;
+      if(pTipo=="desayuno"){
+
+        let alimentos = vm.alimentos1;
+        let alimentosDesayuno = vm.cantidadAlimentosDesayuno;
+        console.log("Alimentos Desayuno =>",alimentosDesayuno);
+        if(alimentosDesayuno.length > 1){
+          let flag= true;
+
+          for(let i=0;i<alimentosDesayuno.length;i++){
+              if(alimentosDesayuno[i].alimento.Id == value){
+                flag=false;
+                break;
+              }             
+          }
+
+          if(flag){
+            for(let i=0;i<alimentos.length;i++){
+              if(alimentos[i].Id == value){
+                for(let j=0;j<alimentosDesayuno.length;j++){
+                  if(alimentosDesayuno[j].key == pKey){
+                    alimentosDesayuno[j].alimento = alimentos[i];
+                    break;
+                  }
+                }
+                break;
+              }
+            }
+          }else{
+            alert("No puede escoger un mismo alimento");
+            return false;
+          }
+
+
+        }else{
+          for(let i=0;i<alimentos.length;i++){
+            if(alimentos[i].Id==value){
+              for(let j=0;j<alimentosDesayuno.length;j++){
+                if(alimentosDesayuno[j].key == pKey){
+                  alimentosDesayuno[j].alimento = alimentos[i];
+                  break;
+                }
+              }
+              break;
+            }
+          }
+        }
+      }
+    },
+    addAlimento: function(tipo){
+      var vm = this;
+      if(tipo=="desayuno"){
+        let key = Math.random() * (100000 - 1) + 1;
+        vm.cantidadAlimentosDesayuno.push({key:key,alimento:{}});
+      }         
+    }
+  },
+  mounted(){
+
+    let self = this;
+
+    axios.get("https://calculadoranutricional.herokuapp.com/api/sexos")
+    .then(function(res){
+      self.sexos = res.data;
+    })
+    .catch(function(err){
+      alert(err);
+    })
+
+    axios.get("https://calculadoranutricional.herokuapp.com/api/rango-edades")
+    .then(function(res){
+      self.rangoEdades = res.data.data;
+    })
+    .catch(function(err){
+      alert(err);
+    });
+
+    axios.get("https://calculadoranutricional.herokuapp.com/api/alimentos")
+      .then(function(res){
+        self.alimentos1 = res.data.data;
+      });
   }
 }
-</script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+</script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
+import vuetify from './plugins/vuetify';
+import 'roboto-fontface/css/roboto/roboto-fontface.css'
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
+import './assets/style.css';
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  icons: {
+    iconfont: 'md',
+  },
+  vuetify,
+  render: h => h(App)
 }).$mount('#app')
